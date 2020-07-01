05bb272f85b0e8fdf9deeb19de9f4accd464fff2
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-01 09:59:12 -0400

    Did some refactoring and changed coding to include double digit numbers

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -4,132 +4,72 @@ function compute(expression) {
     var numberCount = 0;
     var polishMathArray = [];
     var solution = 0;
+    var numberString = "";
 
     for(var index = 0; index < expressionToArray.length; index++){
             var currentToken = expressionToArray[index];
+            numberString += currentToken;
             if(currentToken == "+" || currentToken == "-" || currentToken == "*" || currentToken == "/"){
               mathOperator = currentToken;
-            //  console.log("THE MATH OPERATOR IS " + mathOperator);  
+
+              if(numberCount == 0){ // THIS IS THE VERY FIRST NUMBER ENCOUNTERED IN THE EXPRESISION
+                //   console.log("FOUND THE VERY FIRST NUMBER");
+                   polishMathArray[0] = parseInt(numberString);
+                 //  console.log(polishMathArray[0]);
+                   numberCount++;
+                   numberString = "";
+              }
+              else if(numberCount == 1){
+                //  console.log("FOUND THE 2nd NUMBER");
+                  polishMathArray[1] = parseInt(numberString);
+                 // console.log(polishMathArray[1]);
+                  numberCount++;
+                  numberString = "";
+              }
             }
-            else{
-                if(numberCount == 0){ // THIS IS THE VERY FIRST NUMBER ENCOUNTERED IN THE EXPRESISION
-                 //   console.log("FOUND THE VERY FIRST NUMBER");
-                    polishMathArray[0] = parseInt(currentToken);
-                  //  console.log(polishMathArray[0]);
-                    numberCount++;
-                }
-                else if(numberCount == 1){
-                  //  console.log("FOUND THE 2nd NUMBER");
-                    polishMathArray[1] = parseInt(currentToken);
-                   // console.log(polishMathArray[1]);
-                    numberCount++;
-                }
-        
-               // console.log("numberCount = " + numberCount );
-                //WE HAVE 2 NUMBERS and a math operator
-                if(numberCount == 2 && mathOperator != null){
-                 //   console.log("INSIDE THE MATH OPERATIONS IF")
-                    switch(mathOperator){
-                        case "+":
-                            solution = polishMathArray[0] + polishMathArray [1];
-                          //  console.log("ADDING " + polishMathArray[0] + " " + polishMathArray[1]);
-                            numberCount--;
-                            polishMathArray[0] = solution;
-                           // console.log(solution);
-                            break;
-                        case "-":
-                            solution = polishMathArray[0] - polishMathArray [1];
-                           // console.log("Sub");
-                            numberCount--;
-                            polishMathArray[0] = solution;
-                            //console.log(solution);
-                            break;
-                        case "*":
-                            solution = polishMathArray[0] * polishMathArray [1];
-                           // console.log("MULT");
-                            numberCount--;
-                            polishMathArray[0] = solution;
-                           // console.log(solution);
-                            break;
-                        case "/":
-                            solution = (polishMathArray[0] / polishMathArray [1]);
-                            console.log("DIV");
-                            numberCount--;
-                            polishMathArray[0] = solution;
-                           // console.log(solution);
-                            break;
-                    }
-                }
-            }
+               //WE HAVE 2 NUMBERS and a math operator
+          if(numberCount == 2 && mathOperator != null){
+              solution = doMath(polishMathArray, mathOperator);
+              numberCount--;
+              polishMathArray[0] = solution;
         }
+      }
+        //WE assume that the expression is proper and we need to deal with the last number with the last operator
+        polishMathArray[1] = parseInt(numberString);
+        solution = doMath(polishMathArray, mathOperator);
         return solution;
 }
 
-// var expression = "1+1+1";
-// var expressionToArray = expression.split("");
-// var mathOperator = null;
-// var numberCount = 0;
-// var polishMathArray = [];
-// var solution = 0;
+function doMath(numberArray, mathOP){
+  console.log("DOING MATH");
+  console.log("NUMBER 1 " + numberArray[0] + " NUMBER 2 " + numberArray[1]);
+  var currentSolution = 0;
+  switch(mathOP){
+    case "+":
+      currentSolution = numberArray[0] + numberArray [1];
+        console.log("Add"); 
+         // console.log(solution);
+        break;
+    case "-":
+      currentSolution = numberArray[0] - numberArray [1];
+        console.log("Sub");  
+        //console.log(solution);
+        break;
+    case "*":
+      currentSolution = numberArray[0] * numberArray [1];
+        console.log("MULT");
+         // console.log(solution);
+          break;
+      case "/":
+        currentSolution = (numberArray[0] / numberArray [1]);
+          console.log("DIV");
+         // console.log(solution);
+          break;
+  }
+  console.log("CURRENT SOLUTION IS " + currentSolution);
+  return currentSolution;
+}
 
-// for(var index = 0; index < expressionToArray.length; index++){
-//     var currentToken = expressionToArray[index];
-//     if(currentToken == "+" || currentToken == "-" || currentToken == "*" || currentToken == "/"){
-//       mathOperator = currentToken;
-//       console.log("THE MATH OPERATOR IS " + mathOperator);  
-//     }
-//     else{
-//         if(numberCount == 0){ // THIS IS THE VERY FIRST NUMBER ENCOUNTERED IN THE EXPRESISION
-//             console.log("FOUND THE VERY FIRST NUMBER");
-//             polishMathArray[0] = parseInt(currentToken);
-//             console.log(polishMathArray[0]);
-//             numberCount++;
-//         }
-//         else if(numberCount == 1){
-//             console.log("FOUND THE 2nd NUMBER");
-//             polishMathArray[1] = parseInt(currentToken);
-//             console.log(polishMathArray[1]);
-//             numberCount++;
-//         }
-
-//         console.log("numberCount = " + numberCount );
-//         //WE HAVE 2 NUMBERS and a math operator
-//         if(numberCount == 2 && mathOperator != null){
-//             console.log("INSIDE THE MATH OPERATIONS IF")
-//             switch(mathOperator){
-//                 case "+":
-//                     solution = polishMathArray[0] + polishMathArray [1];
-//                     console.log("ADDING " + polishMathArray[0] + " " + polishMathArray[1]);
-//                     numberCount--;
-//                     polishMathArray[0] = solution;
-//                    // console.log(solution);
-//                     break;
-//                 case "-":
-//                     solution = polishMathArray[0] - polishMathArray [1];
-//                     console.log("Sub");
-//                     numberCount--;
-//                     polishMathArray[0] = solution;
-//                     //console.log(solution);
-//                     break;
-//                 case "*":
-//                     solution = polishMathArray[0] * polishMathArray [1];
-//                     console.log("MULT");
-//                     numberCount--;
-//                     polishMathArray[0] = solution;
-//                    // console.log(solution);
-//                     break;
-//                 case "/":
-//                     solution = polishMathArray[0] / polishMathArray [1];
-//                     console.log("DIV");
-//                     numberCount--;
-//                     polishMathArray[0] = solution;
-//                    // console.log(solution);
-//                     break;
-//             }
-//         }
-//     }
-// }
-// console.log(solution);
 
 
     
